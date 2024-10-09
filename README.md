040972cd345a890e1e9b4526231c160da1a3241c
Mark Paulin <mark.paulin@nisra.gov.uk>
2024-10-09 14:44:47 +0100

    add readme and support for config

---
+++ README.md
@@ -0,0 +1,12 @@
+# skiql
+
+skiql is a small python package for running a bunch of SQL files. This is to solve that issue where you have a lot of SQL files for functions, views, stored procedures and whatever else, you change the name of a column in a single table, then you need to follow that the whole way through and make sure everything has been synced up. So why not just run everything.
+
+## Installation
+
+Open a terminal in this directory and run `py -m pip install .` to install.
+
+## Usage
+
+Command line options etc.
+Should I set up configuration files? Check how eg SQLFluff does it?
--- src/skiql/cli.py
+++ src/skiql/cli.py
@@ -1,11 +1,36 @@
+import tomllib
+
 import click
 
 from skiql.run_files import run_all_files
 
+DEFAULT_CFG = ".skiql"
+
+
+def configure(ctx, param, filename):
+    try:
+        with open(filename, "rb") as file:
+            cfg = tomllib.load(file)
+            options = dict(cfg["options"])
+    except (KeyError, FileNotFoundError):
+        options = {}
+    ctx.default_map = options
+
 
 @click.command()
 @click.argument("folders", nargs=-1)
-@click.option("--connection_string", help="Connection string")
+@click.option(
+    "-c",
+    "--config",
+    type=click.Path(dir_okay=False),
+    default=DEFAULT_CFG,
+    callback=configure,
+    is_eager=True,
+    expose_value=False,
+    help="Read option defaults from the specified toml file",
+    show_default=True,
+)
+@click.option("--connection_string", required=True, help="Connection string")
 @click.option("--max_tries", default=2, help="Maximum number of retries")
 def cli(folders, connection_string, max_tries):
     failed_files = run_all_files(folders, connection_string, max_tries)
